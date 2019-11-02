85360a261fb33030d19044f9b0603814d0536ba6
Misty Lackie <mistylackie@users.noreply.github.com>
2019-11-02 14:19:17 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('AWS_KEY', 'your-aws-key-here');
+define('AWS_SECRET_KEY', 'your-aws-secret-here');
+define('AWS_API_VERSION', '2006-03-01');
+define('HOST', 'your-host-here.s3.amazonaws.com');
+define('REGION', 'us-east-1');
+define('BUCKET', 'your-s3-bucket-here');
